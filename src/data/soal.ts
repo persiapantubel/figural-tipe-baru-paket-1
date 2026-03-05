51da870d07a00b8163ad6aab2b808f63beb4fae7
persiapantubel <spmbtubel@gmail.com>
2026-03-05 13:13:43 +0700

    Update soal.ts

--- src/data/soal.ts
+++ src/data/soal.ts
@@ -105,9 +105,9 @@ export const soalData: Soal[] = [
       { center: { t: 'segi7', f: true }, tl: { t: 'diamond', f: false }, tr: { t: 'star', f: true }, bl: { t: 'square', f: false }, br: { t: 'segi3', f: true } },
     ],
     options: [
-      { label: 'A', center: { t: 'segi8', f: false }, tl: { t: 'diamond', f: false }, tr: { t: 'circle', f: true }, bl: { t: 'square', f: false }, br: { t: 'star', f: true } },
-      { label: 'B', center: { t: 'segi8', f: false }, tl: { t: 'diamond', f: false }, tr: { t: 'circle', f: true }, bl: { t: 'square', f: false }, br: { t: 'star', f: true } },
-      { label: 'C', center: { t: 'segi8', f: false }, tl: { t: 'diamond', f: false }, tr: { t: 'circle', f: true }, bl: { t: 'square', f: false }, br: { t: 'star', f: true } },
+      { label: 'A', center: { t: 'segi8', f: false }, tl: { t: 'diamond', f: false }, tr: { t: 'segi3', f: true }, bl: { t: 'square', f: false }, br: { t: 'star', f: true } },
+      { label: 'B', center: { t: 'segi8', f: false }, tl: { t: 'square', f: false }, tr: { t: 'circle', f: true }, bl: { t: 'square', f: false }, br: { t: 'star', f: true } },
+      { label: 'C', center: { t: 'segi8', f: false }, tl: { t: 'star', f: false }, tr: { t: 'circle', f: true }, bl: { t: 'square', f: false }, br: { t: 'star', f: true } },
       { label: 'D', center: { t: 'segi8', f: false }, tl: { t: 'diamond', f: false }, tr: { t: 'diamond', f: true }, bl: { t: 'square', f: false }, br: { t: 'star', f: true } },
       { label: 'E', center: { t: 'segi8', f: false }, tl: { t: 'diamond', f: true }, tr: { t: 'circle', f: false }, bl: { t: 'square', f: true }, br: { t: 'star', f: false } },
     ]
